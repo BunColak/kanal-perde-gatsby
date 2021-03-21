423a2e979e37bf24445ec5c5e62c7e94926961a7
Bun Colak <bun.colak@motius.de>
2021-03-21 14:56:36 +0100

    youtube links for announcements

--- src/layouts/AnnouncementLayout.js
+++ src/layouts/AnnouncementLayout.js
@@ -4,6 +4,7 @@ import { graphql } from "gatsby"
 import styled from "styled-components"
 import SmallDate from "../components/SmallDate"
 import BlockText from "../components/BlockText"
+import IframeResizer from "iframe-resizer-react"
 
 const Container = styled.div`
   display: flex;
@@ -17,11 +18,15 @@ const Container = styled.div`
     width: 75%;
     border-radius: 4px;
   }
-  
+
   h2 {
     margin-top: 1.5rem;
     margin-bottom: 0.5rem;
   }
+`
+
+const YoutubeContainer = styled(IframeResizer)`
+  margin-top: 1rem;
 `
 
 const AnnouncementLayout = ({ data: { sanityAnnouncement: announcement } }) => {
@@ -32,6 +37,17 @@ const AnnouncementLayout = ({ data: { sanityAnnouncement: announcement } }) => {
         <h2>{announcement.title}</h2>
         <SmallDate>{new Intl.DateTimeFormat("tr").format(new Date(announcement.date))}</SmallDate>
         <BlockText block={announcement.description} />
+        {announcement.link &&
+        <YoutubeContainer
+          log
+          src={`https://www.youtube.com/embed/${announcement.link}`}
+          style={{
+            width: "1px",
+            minWidth: "100%",
+            minHeight: "100%",
+            height: window.innerWidth > 1000 ? "450px" : "200px"
+          }}
+        />}
       </Container>
     </Layout>
   )
@@ -44,6 +60,7 @@ export const query = graphql`
       title
       date
       slug
+      link
       description {
         _key
         _type
